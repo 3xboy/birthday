b5b85024da065bbd186f5af51eb4613a1b97314b
izhc <145135993+zah-star@users.noreply.github.com>
2024-03-15 12:17:03 +0800

    update2

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "好像少点东西",
+        bannar_coming: "好像少点什么",
         balloons_flying: "还有...",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
